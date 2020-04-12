6c25135df1c6f720de843beae61e9b37dab0b5ab
Carlos Ricci <roberto.ricci@sonepar.com.br>
2020-04-12 17:19:36 -0300

    Finalização do Front

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post("repositories", {
+        title: `GoStack-${Date.now()}`,
+        url: "https://github.com/robertoricci/desafio-conceitos-reactjs",
+        techs: "['Node.js', 'React Native', 'ReactJS','Mondo Db']",
+      });
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+    } catch (err) {
+      alert("Erro ao adicionar repositório, tente novamente");
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+
+      setRepositories(
+        repositories.filter((repository) => repository.id !== id)
+      );
+    } catch (err) {
+      alert("Erro ao deletar repositório, tente novamente");
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            <h3>{repository.title}</h3>
+            <h4>{repository.techs}</h4>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
